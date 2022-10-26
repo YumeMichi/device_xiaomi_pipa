fae9dd5200c50e914136ee95a1037df7c8bc42bc
cjybyjk <cjybyjk@zjnu.edu.cn>
2022-10-26 12:23:45 +0800

    pipa: parts: Adapt for new Xiaomi Keyboard
     #* Lynnrin: Dagu using different keyboard between nabu/elish, so this keyboard implement won't work on dagu
    
    Co-authored-by: KaguraRinko <lynnrin@lynnrin.moe>
    Change-Id: I35ea13e0a3c26d63b73081a7ac8fe5b0518fc09b

--- parts/src/org/lineageos/settings/BootCompletedReceiver.java
+++ parts/src/org/lineageos/settings/BootCompletedReceiver.java
@@ -26,6 +26,7 @@ import android.util.Log;
 import org.lineageos.settings.thermal.ThermalUtils;
 import org.lineageos.settings.refreshrate.RefreshUtils;
 import org.lineageos.settings.utils.FileUtils;
+import org.lineageos.settings.keyboard.XiaomiKeyboardUtils;
 
 import vendor.xiaomi.hardware.touchfeature.V1_0.ITouchFeature;
 
@@ -51,9 +52,6 @@ public class BootCompletedReceiver extends BroadcastReceiver {
             mTouchFeature.setTouchMode(20, stylus_prefs.getInt(SHARED_STYLUS, 0));
         } catch (Exception e) {
         }
-        if (keyboard_prefs.getInt(SHARED_KEYBOARD, 1) == 0)
-            FileUtils.writeLine("/sys/devices/platform/soc/soc:xiaomi_keyboard/xiaomi_keyboard_conn_status", "enable_keyboard");
-        else
-            FileUtils.writeLine("/sys/devices/platform/soc/soc:xiaomi_keyboard/xiaomi_keyboard_conn_status", "disable_keyboard");
+        XiaomiKeyboardUtils.startService(context);
     }
 }
