b9da7935430dbe386e9c67102c19ad6b801d42ac
Anthony Adamo <quic_aadamo@quicinc.com>
2023-01-05 14:55:38 -0800

    pipa: Add aidl bootcontrolhal to PRODUCT_PACKAGES
    add in the aidl bootcontrol hal server implementation to
    the PRODUCT PACKAGES
    
    Change-Id: I481e337b7d92d4a4d797a1e5dee695b39159f987

--- device.mk
+++ device.mk
@@ -117,9 +117,8 @@ PRODUCT_SYSTEM_EXT_PROPERTIES += \
 
 # Boot
 PRODUCT_PACKAGES += \
-    android.hardware.boot@1.2-impl-qti \
-    android.hardware.boot@1.2-impl-qti.recovery \
-    android.hardware.boot@1.2-service
+    android.hardware.boot-service.qti \
+    android.hardware.boot-service.qti.recovery
 
 # Camera
 PRODUCT_VENDOR_PROPERTIES += \
