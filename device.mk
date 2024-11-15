2d7569db81facdf2ba16458d9204aaae341d205c
Sean Du <do4suki@gmail.com>
2024-11-16 05:25:56 +0800

    pipa: Rename telephony to remove_packages
    Change-Id: Ibbda20f6fa6426a9b4f12c6adce499e1a7008585
    Signed-off-by: Sean Du <do4suki@gmail.com>

--- device.mk
+++ device.mk
@@ -352,7 +352,7 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/tablet_core_hardware.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/tablet_core_hardware.xml
 
 PRODUCT_PACKAGES += \
-    RemoveTelephonyPackages
+    RemovePackages
 
 PRODUCT_PRODUCT_PROPERTIES += \
     persist.debug.disable_lid_state=true
