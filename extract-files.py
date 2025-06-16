f12f7f0b09296d8dcd5a67e7fcce138e429e3c0f
Bin <9633157+web1n@users.noreply.github.com>
2025-06-16 23:40:24 +0800

    pipa: Patch audio to load libaudioroute-v34.so
    Change-Id: Idad20512982b8753820fffa6d956df137e462638

--- extract-files.py
+++ extract-files.py
@@ -47,6 +47,11 @@ lib_fixups: lib_fixups_user_type = {
 blob_fixups: blob_fixups_user_type = {
     'odm/bin/hw/vendor.dolby_v3_6.hardware.dms360@2.0-service': blob_fixup()
         .add_needed('libstagefright_foundation-v33.so'),
+    'vendor/lib/libaudioroute_ext.so': blob_fixup()
+        .replace_needed(
+            'libaudioroute.so',
+            'libaudioroute-v34.so',
+        ),
     'vendor/lib/hw/audio.primary.pipa.so': blob_fixup()
         .binary_regex_replace(
             b'/vendor/lib/liba2dpoffload\\.so',
@@ -55,6 +60,10 @@ blob_fixups: blob_fixups_user_type = {
         .binary_regex_replace(
             b'/vendor/lib/libssrec\\.so',
             b'libssrec_pipa.so\x00\x00\x00\x00\x00\x00\x00',
+        )
+        .replace_needed(
+            'libaudioroute.so',
+            'libaudioroute-v34.so',
         ),
     (
         'vendor/lib64/libcodec2_soft_ac4dec.so',
