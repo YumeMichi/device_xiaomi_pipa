royal-jaat/aospa_device_xiaomi_pipa

commit 73c0b0629a58e4d09f4b967d86e64783e6984fe1

sm8250-common: media: Remove OMX
 * OMX HW video codecs are no longer used after enabling Codec2
 * OMX HW audio codecs won't work without QCOM A/V enhancements, which
   we don't have on Lineage.

Change-Id: I65852151c8fb6387add5fc4402ba3f13c97b5f64

device.mk
@@ -223,6 +223,12 @@ PRODUCT_PACKAGES += \
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.vendor.lmk.force_inkernel_lmk=true
 
+# Media
+TARGET_SUPPORTS_OMX_SERVICE := false
+
+PRODUCT_VENDOR_PROPERTIES += \
+    vendor.media.omx=0
+
 # Multi-Gen LRU
 PRODUCT_SYSTEM_EXT_PROPERTIES += \
     persist.device_config.mglru_native.lru_gen_config=core_and_mm_walk

configs/vintf/manifest.xml
@@ -64,20 +64,6 @@ IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
             <instance>default</instance>
         </interface>
     </hal>
-    <hal format="hidl">
-        <name>android.hardware.media.omx</name>
-        <transport>hwbinder</transport>
-        <impl level="generic"></impl>
-        <version>1.0</version>
-        <interface>
-            <name>IOmx</name>
-            <instance>default</instance>
-        </interface>
-        <interface>
-            <name>IOmxStore</name>
-            <instance>default</instance>
-        </interface>
-    </hal>
     <!-- VPP Service -->
     <hal format="hidl">
         <name>vendor.qti.hardware.vpp</name>

YumeMichi/device_xiaomi_pipa

commit 6404b7b9dd78cfadd90ed0e17f5e5c277129c802

pipa: Move OMX service flag to product config
The product configuration reads this flag before BoardConfig.mk or
device.mk is included. The flag now lives in vendor/aospa/products/pipa/
aospa_pipa.mk before aosp_base.mk is inherited.

Reference commit: aospa-pipa/android_vendor_aospa@2a3700a157ab

Fixes: 73c0b0629a58 ("sm8250-common: media: Remove OMX")
Change-Id: I4f80387507979874bc74370da71ee1fb10b654ff
Signed-off-by: Sean Du <do4suki@gmail.com>

device.mk
@@ -255,8 +255,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.vendor.lmk.force_inkernel_lmk=true
 
 # Media
-TARGET_SUPPORTS_OMX_SERVICE := false
-
 PRODUCT_VENDOR_PROPERTIES += \
     debug.stagefright.c2inputsurface=-1 \
     ro.odm.build.media_performance_class=30 \